ad7d691f2fb4ba7e063085bd48f4127a734ba977
soetakara <soetakara.717@gmail.com>
2021-11-01 08:09:33 +0900

    usersテーブルの編集

--- db/migrate/20211031230405_devise_create_users.rb
+++ db/migrate/20211031230405_devise_create_users.rb
@@ -7,6 +7,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      ##users table
+      t.string :nickname,           null: false, unique: true
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
+      t.string :last_name_kana,     null: false
+      t.string :first_name_kana,    null: false
+      t.date   :birthday,           null: false
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
