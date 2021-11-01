ed6b94bb6cb7f1ece382c8b89966a27b2be9bed3
soetakara <soetakara.717@gmail.com>
2021-11-02 07:15:54 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  ##バリデーションの設定
+  validates :nickname,         precense: true
+  validates :last_name,        precense: true
+  validates :first_name,       precense: true
+  validates :last_name_kana,   precense: true
+  validates :first_name_kana,  precense: true
+  validates :birthday,         precense: true
+
+
 end
