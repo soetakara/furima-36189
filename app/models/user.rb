bd82629388a2891d7eb6c9515ca612ca7fa69416
soetakara <soetakara.717@gmail.com>
2021-11-03 10:58:18 +0900

    userモデルのバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   ##バリデーションの設定
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "Password is invalid. Include both letters and numbers"}
   validates :nickname,         presence: true
-  validates :last_name,        presence: true
-  validates :first_name,       presence: true
-  validates :last_name_kana,   presence: true
-  validates :first_name_kana,  presence: true
   validates :birthday,         presence: true
+
+  with_options presence: true do
+    validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Input full-width characters"}
+    validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Input full-width characters"}
+    validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "Input full-width katakana characters"}
+    validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "Input full-width katakana characters"}
+  end
 
 
 end
--- db/migrate/20211031230405_devise_create_users.rb
+++ db/migrate/20211031230405_devise_create_users.rb
@@ -4,11 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false, unique: true, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ##users table
-      t.string :nickname,           null: false, unique: true
+      t.string :nickname,           null: false
       t.string :last_name,          null: false
       t.string :first_name,         null: false
       t.string :last_name_kana,     null: false
