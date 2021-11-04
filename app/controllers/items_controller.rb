447c110d481b512b3be3bf4151efcb105e287d53
soetakara <soetakara.717@gmail.com>
2021-11-05 07:27:23 +0900

    サインインの条件分岐の変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,15 @@
 class ItemsController < ApplicationController
+  before_action :sign_in_check, only: [:new]
   def index
   end
 
   def new
+    @item = Item.new
+  end
+
+  private
+  def sign_in_check
     if user_signed_in?
-      @item = Item.new
     else
       redirect_to new_user_session_path
     end
