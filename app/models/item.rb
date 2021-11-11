2c55e6562b59b7ace6e7d1d543143c46ed3c597a
soetakara <soetakara.717@gmail.com>
2021-11-11 23:29:42 +0900

    単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,19 @@
 class Item < ApplicationRecord
   ## バリデーションの設定
   validates :title,            presence: true
-  validates :category_id,      numericality: {other_than: 1, message: " can't be blank"}
-  validates :status_id,        numericality: {other_than: 1, message: " can't be blank"}
-  validates :fee_id,           numericality: {other_than: 1, message: " can't be blank"}
-  validates :prefecture_id,    numericality: {other_than: 1, message: " can't be blank"}
-  validates :shipping_date_id, numericality: {other_than: 1, message: " can't be blank"}
+  validates :category_id,      numericality: {other_than: 1, message: "can't be blank"}
+  validates :status_id,        numericality: {other_than: 1, message: "can't be blank"}
+  validates :fee_id,           numericality: {other_than: 1, message: "can't be blank"}
+  validates :prefecture_id,    numericality: {other_than: 1, message: "can't be blank"}
+  validates :shipping_date_id, numericality: {other_than: 1, message: "can't be blank"}
   # validates :price,            presence: true
   validates :explantion,       presence: true
 
   validates :image,            presence: true
 
   with_options presence: true do
-    validates :price,          numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Price is out of setting range"}
-    validates :price,          format: {with: /\A[0-9]+\z/, message: " is invalid. Input half-width characters"}
+    validates :price,          numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+    validates :price,          format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters"}
   end
 
   ##アソシエーションの設定
