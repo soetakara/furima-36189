725526d43d67521b2f7f393b7396d198cda6cb63
soetakara <soetakara.717@gmail.com>
2021-11-13 11:27:48 +0900

    エラー文重複の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,8 +12,7 @@ class Item < ApplicationRecord
   validates :image,            presence: true
 
   with_options presence: true do
-    validates :price,          numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
-    validates :price,          format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters"}
+    validates :price,          numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters"}
   end
 
   ##アソシエーションの設定
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -78,7 +78,6 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is out of setting range")
       end
-      
     end
   end
 end
