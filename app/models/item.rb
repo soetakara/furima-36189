1d3dd759a5a739060c285389d464525355feb457
soetakara <soetakara.717@gmail.com>
2021-11-13 11:59:16 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,6 @@ class Item < ApplicationRecord
   validates :fee_id,           numericality: {other_than: 1, message: "can't be blank"}
   validates :prefecture_id,    numericality: {other_than: 1, message: "can't be blank"}
   validates :shipping_date_id, numericality: {other_than: 1, message: "can't be blank"}
-  # validates :price,            presence: true
   validates :explantion,       presence: true
 
   validates :image,            presence: true
