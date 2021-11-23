2fd1029c50f439c0ec01efdedd5cdf50515230fc
soetakara <soetakara.717@gmail.com>
2021-11-23 21:50:25 +0900

    バリデーション設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,11 @@
 class Address < ApplicationRecord
+  validates :postal_code,      presence: true
+  validates :prefecture_id,    numericality: {other_than: 1, message: "can't be blank"}
+  validates :municipality,     presence: true
+  validates :house_number,     presence: true
+  validates :building_name
+  validates :telephone_number, presence: true
+
   belongs_to :order
   belongs_to :prefecture
 end
