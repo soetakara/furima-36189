e9e9e054148c1baae9c48ae88b273b8f1ce5d3f6
soetakara <soetakara.717@gmail.com>
2021-11-23 21:12:09 +0900

    決済機能の実装（環境変数実装前）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,32 @@
 class OrdersController < ApplicationController
   def index
+    @order = Order.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    binding.pry
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_7fe5f160d1873c77b83696a8"
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy' 
+    )
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
+  attr_accessor :token
+  validates :token, presence: true
+
   belongs_to :user
   belongs_to :item
 end
