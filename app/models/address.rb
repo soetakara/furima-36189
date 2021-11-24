0b5054c9929d11aac2619867d902d37720f3fc99
soetakara <soetakara.717@gmail.com>
2021-11-24 20:16:43 +0900

    addressへのバリデーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,12 +1,10 @@
 class Address < ApplicationRecord
-  validates :postal_code,      presence: true
+  validates :postal_code,      presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :prefecture_id,    numericality: {other_than: 1, message: "can't be blank"}
   validates :municipality,     presence: true
   validates :house_number,     presence: true
   validate  :building_name
-  validates :telephone_number, presence: true
-
-  # with 
+  validates :telephone_number, presence: true, format: {with: /\A[0-9]+\z/, greater_than_or_equal_to: 10, less_than_or_equal_to: 11}
 
   belongs_to :order
 
