602562347fe2ddd1d29132a0b227cb01c4c89a9b
soetakara <soetakara.717@gmail.com>
2021-11-25 23:31:04 +0900

    orderコントローラにitemのbefore_action変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,8 @@
 class OrdersController < ApplicationController
+  before_action :set_item
+
   def index
     @order_address = OrderAddress.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
@@ -16,6 +17,10 @@ class OrdersController < ApplicationController
   end
 
   private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
   def order_address_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :municipality, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
