398817183daa049bc13ff2d6c4c86d534e1fae11
soetakara <soetakara.717@gmail.com>
2021-11-25 22:04:44 +0900

    インスタンス変数、モデル名の修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,15 @@
 class OrdersController < ApplicationController
   def index
-    @order = Order.new
+    @order = OrderAddress.new
     @item = Item.find(params[:item_id])
   end
 
   def create
-    binding.pry
-    @order = Order.new(order_params)
-    if @order.valid?
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.valid?
       # pay_item
-      @order.save
-      Address.create(address_params)
+      @order_address.save
+      # Address.create(address_params)
       return redirect_to root_path
     else
       render 'index'
@@ -18,13 +17,13 @@ class OrdersController < ApplicationController
   end
 
   private
-  def order_params
-    params.require(:order).permit().merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  def order_address_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :municipality, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
-  def address_params
-    params.require(:order).permit(:postal_code, :prefecture_id, :municipality, :house_number, :building_name, :telephone_number).merge(order_id: @order.id)
-  end
+  # def address_params
+  #   params.require(:order).permit(:postal_code, :prefecture_id, :municipality, :house_number, :building_name, :telephone_number).merge(order_id: @order.id)
+  # end
 
   def pay_item
     Payjp.api_key = "sk_test_7fe5f160d1873c77b83696a8"
